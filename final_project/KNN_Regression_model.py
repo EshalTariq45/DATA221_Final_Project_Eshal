2b62e62bc200c769bd860ef1ab44b9b47c89a071
EshalTariq45 <eshal.tariq@ucalgary.ca>
2026-03-18 17:58:25 -0600

    removing columns from data frame (G1 and G2) and dropping G3 from x, since we're training our model

--- final_project/KNN_Regression_model.py
+++ final_project/KNN_Regression_model.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 #loading dataset
 df= pd.read_csv("student-mat.csv")
+
+#dropping the leakage features
+
+#removing two columns from dataframe: G1 and G2
+df=df.drop(columns=["G1", "G2"])
+
+#seperating features and targets
+#dropping column G3 from x, otherwise our model would 'see' the answer while
+#we're training it.
+x= df.drop("G3", axis=1) #features only
+y=df["G3"] #target
