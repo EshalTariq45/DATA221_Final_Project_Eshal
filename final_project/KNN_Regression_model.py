9b4803c6f73085f0da8bb5ac6c5c1d4a989d1a00
EshalTariq45 <eshal.tariq@ucalgary.ca>
2026-03-18 18:21:18 -0600

    made a small comment beside x=df.drop for better readability

--- final_project/KNN_Regression_model.py
+++ final_project/KNN_Regression_model.py
@@ -11,5 +11,5 @@ df=df.drop(columns=["G1", "G2"])
 #seperating features and targets
 #dropping column G3 from x, otherwise our model would 'see' the answer while
 #we're training it.
-x= df.drop("G3", axis=1) #features only
+x= df.drop("G3", axis=1) #features only, axis=1: operating along columns instead of rows
 y=df["G3"] #target
