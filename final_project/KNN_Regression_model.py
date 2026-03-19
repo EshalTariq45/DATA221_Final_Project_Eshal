95825e09235925aeb26654f49b21d25d0586458e
EshalTariq45 <eshal.tariq@ucalgary.ca>
2026-03-19 12:49:30 -0600

    printing results and sample predictions

--- final_project/KNN_Regression_model.py
+++ final_project/KNN_Regression_model.py
@@ -88,5 +88,10 @@ mse=mean_squared_error(y_test, y_pred)
 rmse= np.sqrt(mse)
 r2= r2_score(y_test, y_pred)
 
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2 Score:", r2)
 
+print("Sample Predictions:", y_pred[:5])
 
