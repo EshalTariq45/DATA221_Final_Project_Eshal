40d1b9a45353b058a45602b6b1c809ec91988369
EshalTariq45 <eshal.tariq@ucalgary.ca>
2026-03-19 12:16:16 -0600

    added more comments as well as one-hot encoding

--- final_project/KNN_Regression_model.py
+++ final_project/KNN_Regression_model.py
@@ -5,20 +5,23 @@ df= pd.read_csv("student-mat.csv")
 
 #dropping the leakage features
 
-#removing two columns from dataframe: G1 and G2
+#removing two columns from dataframe: G1 and G2 (these are earlier grades and highly correlated with G3)
+#   since keeping them would make the model 'cheat' in a sense
 df=df.drop(columns=["G1", "G2"])
 
 
-#Selecting features
+#Selecting features- to keep it focused on our proposal question
 selected_features= ["Medu", "Fedu", "Mjob", "Fjob", #Parents background
                     "address", "Pstatus", "traveltime", "internet", #Living conditions
                     'famsup', "famsize", "famrel"] #Family demographics
 
-x=df[selected_features]
-y=df["G3"] #target
+#split features x, and target y
+x=df[selected_features] #inputs (features)
+y=df["G3"] #target - output (final grade)
 
+#one-hot encode categorical features
 #Indentifying categorical columns manually
 categorical_cols= ['Mjob', 'Fjob', 'address', "Pstatus", "famsup", "internet"]
-
-
+x_encoded= pd.get_dummies(x, columns=categorical_cols, drop_first=True)
+#   this converts categorical values into numbers (example: Mjob=teacher -> Mjob_teacher=1)
 
